029f8f9f5d5a9453fbad605f88a0ad5babc87fb8
Satoshi <no@e.mail>
2024-06-22 02:32:15 +0900

    update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,32 @@
 <script lang="ts">
 	import TypeWriter from '$lib/TypeWriter.svelte'
 
-	const texts = ['lorem ipsum', 'dolor sit amet', 'consectetur adipiscing elit']
+	const lorem = [
+		['lorem ipsum', 'dolor sit amet', 'consectetur adipiscing elit'],
+		['Alias mollitia', 'dolore molestias recusandae', 'ullam voluptates dolor voluptatum'],
+		['voluptas ratione', 'nostrum veniam', 'deleniti possimus'],
+		['sequi ipsam', 'architecto delectus', 'ipsum aliquam odit'],
+	]
+
+	let index = 0
+	let texts = $state(lorem[index])!
+
+	const changeTexts = () => {
+		index = (index + 1) % lorem.length
+		texts = lorem[index]!
+	}
 </script>
 
 <div>
+	<button onclick={changeTexts}>Change Texts</button>
 	<TypeWriter {texts} />
 </div>
 
 <style>
 	div {
 		display: flex;
+		flex-direction: column;
+		gap: 1rem;
 		font-size: 3rem;
 		justify-content: center;
 		align-items: center;
